bc275460825dd76b1e76cc05c4b42b1e9f164808
Cyril TSO <cyril.tso2406@gmail.com>
2019-11-02 01:28:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ from NBAScraper import NBAScraper
 import sys
 
 if __name__ == '__main__':
+    # You have to replace the template variables in
+    # the following keys of the dictionnary :
+    # path, csv_path and csv_name in order to
+    # be able to call the scraper successfully.
+    
     scraper_settings = {
         'path': your_webdriver_pathfile,
         'xpath': '/html/body/main/div[2]/div/div[2]/div/div/div[1]/div[1]/div/div/label/select',
